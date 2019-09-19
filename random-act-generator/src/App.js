b16a1cb236c658ef920c3b2c42d856f33e6b4b7f
Ali Dervisoglu <derya.dervisoglu@gmail.com>
2019-09-19 16:54:22 -0700

    TabNav done but semantic-ui doesn't work for some reason

--- random-act-generator/src/App.js
+++ random-act-generator/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Home from './components/Home';
 import './App.css';
+import TabNav from "./components/TabNav.js";
 
 function App() {
   return (
     <div className="App">
+      <TabNav />
       <Home />
     </div>
   );
