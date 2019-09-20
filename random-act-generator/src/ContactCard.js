255fd09134efd449bd3a09831d3ae66183694245
jacobdurst <jdd2007@gmail.com>
2019-09-19 20:57:41 -0400

    created contact card and contact list components

---
+++ random-act-generator/src/ContactCard.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function ContactCard() {
+    return (
+        <div>
+            <Card.Group>
+                <Card>
+                    <Card.Content>
+                        <Card.Header>Name of Contact</Card.Header>
+                        <Card.Meta>Phone Number</Card.Meta>
+                        <Card.Description>You have done X random acts of kindness for them</Card.Description>
+                        <Card.Description extra>They have done X random acts of kindness for you</Card.Description>
+                    </Card.Content>
+                    <Card.Content extra>
+                        <div className='ui two buttons'>
+                            <Button basic color='green'>Suprise them!</Button>
+                        </div>
+                    </Card.Content>
+                </Card>
+            </Card.Group>
+        </div>
+    )
+}
+
+export default function ContactCard;
