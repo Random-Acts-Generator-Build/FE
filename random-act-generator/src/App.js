9c4d35fb4819795c59416dfbae313e1161136ce8
Ali Dervisoglu <derya.dervisoglu@gmail.com>
2019-09-19 17:42:22 -0700

    AppRouter added, Acts Page worked on

--- random-act-generator/src/App.js
+++ random-act-generator/src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import Home from './components/Home';
 import './App.css';
 import TabNav from "./components/TabNav.js";
+import Acts from "./components/Acts.js";
+import { Route } from 'react-router-dom';
+import AppRouter from './components/AppRouter.js';
 
 function App() {
   return (
     <div className="App">
       <TabNav />
-      <Home />
+      <AppRouter />
     </div>
   );
 }
